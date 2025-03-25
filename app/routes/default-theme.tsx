a4d511b2b33e7f75cc870d32e9c129867d00dd88
Jens Erven <jens.erven@gmail.com>
2025-03-25 10:08:20 +0100

    Refactor Tailwind CSS setup: added new Euri-specific styles, updated cspell configuration to include new words, and created default and Shadcn theme routes for showcase of components styling.

---
+++ app/routes/default-theme.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ThemePage() {
+    return (
+        <div className='flex h-screen w-screen items-center justify-center'>
+            <h1 >Default Theme</h1>
+        </div>
+    );
+}
---
+++ app/routes/shadcn-theme.tsx
@@ -0,0 +1,7 @@
+export default function ShadcnThemePage() {
+    return (
+        <div className='flex h-screen w-screen items-center justify-center'>
+            <h1>Shadcn Theme</h1>
+        </div>
+    );
+}
